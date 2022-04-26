27df59c010ca4c170dfdcd54a9c2addb1c2a1fa1
xRave110 <xRave110@protonmail.com>
2022-04-26 08:27:23 +0200

    Add tests and refactoring smart contracts

--- contracts/MultiSigVault.sol
+++ contracts/MultiSigVault.sol
@@ -5,111 +5,153 @@ import "OpenZeppelin/openzeppelin-contracts@4.5.0/contracts/access/Ownable.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.5.0/contracts/access/AccessControl.sol";
 
 contract MultiSigVault is Ownable, AccessControl {
-    
     //ROLES
     //bytes32 public constant DEPLOYER = keccak256("DEPLOYER"); //
     bytes32 public constant SIGNER = keccak256("SIGNER"); //
     uint256 private startTime;
     address[] public addressesToSign;
     uint256 approvalsLimit;
-    
-    struct Transfer{
+
+    struct Transfer {
         address payable _to;
-        uint _amount;
-        uint _txId;
+        uint256 _amount;
+        uint256 _txId;
         address[] _signatures;
     }
-    
+
     address[] signature;
     Transfer[] public transactionLog;
 
-    constructor(){
+    constructor() {
         approvalsLimit = 1;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(SIGNER, msg.sender);
     }
-    
-    event depositDone(uint amount, address indexed depositedTo); //indexed (used for search and query)
-    
-    function grantRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+
+    event depositDone(uint256 amount, address indexed depositedTo); //indexed (used for search and query)
+
+    function grantRole(bytes32 role, address account)
+        public
+        virtual
+        override
+        onlyRole(getRoleAdmin(role))
+    {
         require(DEFAULT_ADMIN_ROLE != role, "Cannot add admin role");
         approvalsLimit += 1;
         super._grantRole(role, account);
     }
 
-    function revokeRole(bytes32 role, address account) public virtual override onlyRole(getRoleAdmin(role)) {
+    function revokeRole(bytes32 role, address account)
+        public
+        virtual
+        override
+        onlyRole(getRoleAdmin(role))
+    {
         require(DEFAULT_ADMIN_ROLE != role, "Cannot add admin role");
-        require(account == msg.sender, "You cannot revoke role from your own account");
+        require(
+            account == msg.sender,
+            "You cannot revoke role from your own account"
+        );
         approvalsLimit -= 1;
         super._revokeRole(role, account);
     }
-     /**
+
+    /**
      * @dev Deposits native currency into the smart contract.
      *
      * Requirements:
      * - Everyone can deposit
      */
-    function deposit() public payable returns (uint){ //paybale allows to use msg.value
+    function deposit() public payable returns (uint256) {
+        //paybale allows to use msg.value
         emit depositDone(msg.value, msg.sender);
         return address(this).balance; // contract reflects the address
     }
-    
-    function removeTransaction(uint txId) private{
-        for(uint i = txId; i < transactionLog.length - 1; i++){
+
+    function removeTransaction(uint256 txId) private {
+        for (uint256 i = txId; i < transactionLog.length - 1; i++) {
             transactionLog[i] = transactionLog[i + 1];
         }
         transactionLog.pop();
     }
-    
-     /**
+
+    /**
      * @dev Withdraws native currency from the smart contract.
      *
      * Requirements:
      * - All signature of the signers are required
      */
-    function withdraw(uint txId) public payable onlyRole(SIGNER) returns (uint){
+    function withdraw(uint256 txId)
+        public
+        payable
+        onlyRole(SIGNER)
+        returns (uint256)
+    {
         //msg.sender is a payable address
         /* 
         address payable toSend = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
         toSend.transfer(amount); */
         require(txId < transactionLog.length);
         address payable toSend = transactionLog[txId]._to;
-        require(address(this).balance >= transactionLog[txId]._amount, "You do not have enough funds");
-        require(approvalsLimit <= transactionLog[txId]._signatures.length, "You do not have enough signatures");
+        require(
+            address(this).balance >= transactionLog[txId]._amount,
+            "You do not have enough funds"
+        );
+        require(
+            approvalsLimit <= transactionLog[txId]._signatures.length,
+            "You do not have enough signatures"
+        );
         toSend.transfer(transactionLog[txId]._amount); // the safest way to withdraw because of built error handlings (revert commend as for required)
         removeTransaction(txId);
         return address(this).balance;
     }
-    
+
     /**
-     * @dev 
+     * @dev
      *
      * Function allows to withdraw small amounts of funds per 1 day
      * Requirements:
      * - 1 day must be elapsed since last witdrawal
      * - only small numbers of ETH (0.02)
      */
-    function withdrawWithTimeConstraints(uint256 amount) public payable onlyRole(SIGNER) returns (uint){
-        require(address(this).balance >= amount, "You do not have enough funds");
-        require(amount < 2*(10**16), "Too big amount");
-        require(block.timestamp > startTime + 20 seconds, "Not enough time passed since last withdrawal");
+    function withdrawWithTimeConstraints(uint256 amount)
+        public
+        payable
+        onlyRole(SIGNER)
+        returns (uint256)
+    {
+        require(
+            address(this).balance >= amount,
+            "You do not have enough funds"
+        );
+        require(amount < 2 * (10**16), "Too big amount");
+        require(
+            block.timestamp > startTime + 20 seconds,
+            "Not enough time passed since last withdrawal"
+        );
         startTime = block.timestamp;
         payable(msg.sender).transfer(amount);
         return address(this).balance;
     }
 
-    function getBalance() public view returns (uint){
+    function getBalance() public view returns (uint256) {
         return address(this).balance; // contract reflects the address
     }
-    
-    function addTransaction(address payable _to, uint _amount) external payable onlyRole(SIGNER){
-        transactionLog.push(Transfer(_to, _amount, transactionLog.length, signature));
+
+    function addTransaction(address payable _to, uint256 _amount)
+        external
+        payable
+        onlyRole(SIGNER)
+    {
+        transactionLog.push(
+            Transfer(_to, _amount, transactionLog.length, signature)
+        );
     }
 
-    function approve(uint txId) public onlyRole(SIGNER){
+    function approve(uint256 txId) public onlyRole(SIGNER) {
         bool approveAllowed = true;
-        for(uint j; j < transactionLog[txId]._signatures.length; j++){
-            if(msg.sender == transactionLog[txId]._signatures[j]){
+        for (uint256 j; j < transactionLog[txId]._signatures.length; j++) {
+            if (msg.sender == transactionLog[txId]._signatures[j]) {
                 approveAllowed = false;
                 break;
             }
--- tests/test_deposit.py
+++ tests/test_deposit.py
@@ -1,37 +1,53 @@
 import pytest
-import brownie
+from brownie import chain, accounts
 
 accountNr = 1
 depositVal = 10**18
 
+
 @pytest.fixture(scope="module")
 def vault(MultiSigVault, accounts):
-    return MultiSigVault.deploy({'from': accounts[0]})
+    return MultiSigVault.deploy({"from": accounts[0]})
 
-@pytest.fixture(scope="module")    
+
+@pytest.fixture(scope="module")
 def deposited(vault, accounts):
-    return vault.deposit({'from': accounts[accountNr], 'value': depositVal})
+    return vault.deposit({"from": accounts[accountNr], "value": depositVal})
+
 
 def test_init_default_admin_role(vault, accounts):
     assert vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), accounts[0]) == 1
 
+
 def test_init_signer_role(vault, accounts):
     assert vault.hasRole(vault.SIGNER(), accounts[0]) == 1
 
+
 def test_deposit(vault, deposited):
     assert vault.balance() == 10**18
+
 
 def test_roles(vault, accounts):
     vault.grantRole(vault.SIGNER(), accounts[accountNr])
     assert vault.hasRole(vault.SIGNER(), accounts[accountNr]) == 1
 
-'''How to check require functions 
+
+"""How to check require functions 
 def test_roles(vault, accounts):
     vault.grantRole(vault.DEFAULT_ADMIN_ROLE(), accounts[accountNr])
-    assert vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), accounts[accountNr]) == 0'''
+    assert vault.hasRole(vault.DEFAULT_ADMIN_ROLE(), accounts[accountNr]) == 0"""
+
 
 def test_adding_transactions(vault, accounts):
-    vault.addTransaction(accounts[3], 5*10**17, {"from": accounts[accountNr]})
-    assert vault.transactionLog.length == 1
-    
+    vault.addTransaction(accounts[3], 5 * 10**17, {"from": accounts[accountNr]})
+    assert vault.transactionLog().length == 1
 
+
+def test_daily_withdrawal(vault, deposited):
+    final_time = chain.time() + 40
+    balance = vault.balance()
+    # 60 * 60 * 24
+    vault.withdrawWithTimeConstraints(1 * (10**16))
+    chain.mine(timestamp=final_time)
+    vault.withdrawWithTimeConstraints(1 * (10**16))
+    assert vault.balance() == int((balance) - (2 * (10**16)))
